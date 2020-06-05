c48744bded9d86fa9f5a88be1d3181dfb4bbdcd9
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-06-05 17:31:36 +0200

    TP3 : Parcours d'un tableau en entiers

---
+++ src/fr/boucles/ExerciceTableauEntier.java
@@ -0,0 +1,32 @@
+package fr.boucles;
+
+public class ExerciceTableauEntier {
+	
+	public static void main(String[] args) {
+		
+		// Déclarez un tableau d’entiers contenant tous les entiers de 1 à 10
+		
+		int [] tableauEntier = {1,2,3,4,5,6,7,8,9,10};
+		
+		//Affichez le premier élément du tableau
+		
+		System.out.println("La premiere valeur du tableau est : " + tableauEntier[0]);
+		
+		//Affichez la longueur du tableau en utilisant la propriété length
+		
+		System.out.println("La longeur du tableau est de : "+ tableauEntier.length);
+		
+		//Affichez le dernier élément du tableau en utilisant la propriété length
+		
+		System.out.println("Le dernier élément du tableau est : "+ tableauEntier[tableauEntier.length-1]);
+		
+		//Modifiez la valeur de l’élément d’index 4 et donnez lui la valeur 8.
+		
+		tableauEntier[4] = 6;
+		
+		System.out.println("Le nouveau tableau est " + tableauEntier);
+		
+	}
+
+
+}
