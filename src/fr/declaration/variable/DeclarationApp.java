016affd02b4cbb68eb30bc812196973ce110bcb6
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-06-05 15:22:14 +0200

    Prise en main du debugger et des differents types de variables

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -4,5 +4,31 @@ public class DeclarationApp {
 	
 	public static void main(String[] args) {
 		
+		byte a = 2;
+		short b = 1;
+		int c = 3;
+		long d = 10000;
+		float e = 12;
+		double f = 1.2;
+		char g = 'a';
+		boolean h =true;
+		String i = "Hello";
+		
+		String randomString = "Voici le résultat d’un calcul : \n 1+5=6";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+		
+		System.out.println(randomString);
+
 	}
+	
+	
 }
